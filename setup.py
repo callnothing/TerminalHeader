fc175be851864e676f38f81606052897ce9434ac
Alang Wang <wangchengchao115@gmail.com>
2016-01-27 15:08:57 +0800

    [fix] 修复打包到pypi源问题

--- setup.py
+++ setup.py
@@ -5,19 +5,14 @@ import os
 
 setup(
     name="TerminalHeader",
-    version="0.10",
+    version="0.30",
     description="Add File Header use Terminal",
     author="Alan Wang",
     author_email="wangchengchao115@gmail.com",
     license="GPL",
     packages=find_packages(),
     include_package_data=True,
-    data_files=[
-        ('theader/etc', ['theader/etc/FileHeader.default']),
-        # ('theader/template', ['theader/template']),
-    ],
-    package_data={'': ['*.tmpl']},
-    #   'theader/template/body': ['*.tmpl', 'theader/template/body/*.tmpl']},
+    package_data={'': ['*.tmpl', 'FileHeader.default']},
     entry_points={
         'console_scripts': [
             'theader = theader.__main__:main',
